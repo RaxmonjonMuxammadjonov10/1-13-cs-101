c8ba696d19db6fe1c6854b5fe9109939fffff41c
r.muxammadjonov <r.muxammadjonvo@newuu.uz>
2024-09-11 12:19:08 +0500

    problem 4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 int main() {
- cout<<"I’m standing” on the edge of some crazy cliff.\n What I have to do, I have to catch everybody\n if they start to go over the cliff—I mean\n if they’re running and they don’t look\n where they’re going I have to come out\n from somewhere and catch them.";
-    return 0;
+     int a=189;
+     char b='B';
+     double a4;
+     a4=a/4.0;
+     int asciib='B'-0;
+     float ab5;
+     ab5=(a+asciib)/5;
+     char cc=char('B'+a/3-7);
+     cout<<a<<endl;
+     cout<<b<<endl;
+     cout<<asciib<<endl;
+     cout<<a4<<endl;
+     cout<<ab5<<endl;
+     cout<<cc<<endl;
+     return 0;
+
 }
