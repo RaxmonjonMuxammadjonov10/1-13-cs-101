6a366de7f8ced873112fb521b2fec2a2df8b7a24
r.muxammadjonov <r.muxammadjonvo@newuu.uz>
2024-09-11 12:41:45 +0500

    problem 11

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-     double d,e,p;;
-     cin>>d>>e>>p;
+     double d=1.1333,e=411e-4,p;;
+     //cin>>d>>e>>p;
      //cout<<"Product="<<12*38;
-     cout<<d*e*p;
+     cout<<d<<double(e);
      return 0;
 
 }
