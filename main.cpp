169925abf84fbc35bd14182f086bb1c495d67234
r.muxammadjonov <r.muxammadjonvo@newuu.uz>
2024-09-11 12:21:33 +0500

    problem 5

--- main.cpp
+++ main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-     int a=189;
-     char b='B';
-     double a4;
-     a4=a/4.0;
-     int asciib='B'-0;
-     float ab5;
-     ab5=(a+asciib)/5;
-     char cc=char('B'+a/3-7);
-     cout<<a<<endl;
-     cout<<b<<endl;
-     cout<<asciib<<endl;
-     cout<<a4<<endl;
-     cout<<ab5<<endl;
-     cout<<cc<<endl;
+     cout<<"Product="<<12*38;
+     cout<<"\nSum="<<12+38;
      return 0;
 
 }
