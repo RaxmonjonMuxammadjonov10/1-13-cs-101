6f0a6cedb1b58de1f55eeb7b1b7782cae0b1449c
r.muxammadjonov <r.muxammadjonvo@newuu.uz>
2024-09-11 12:32:35 +0500

    problem 7

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-     int a,b;
+     double a,b;
      cin>>a>>b;
      //cout<<"Product="<<12*38;
-     cout<<"\nSum of "<<a<<" and "<<b<<" = "<<a+b;
+     cout<<"t="<<3.0*(a*a+3.0)/(b/4.0+3.0);
      return 0;
 
 }
